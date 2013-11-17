7bb6ecf0560cf9fb01c79cf5c23bdd170f00e3ee
idz <idz@iosdeveloperzone.com>
2013-11-16 16:11:46 -0800

    Added MIT license to source files. Renamed view controller class. Renamed storyboard.

---
+++ IDZWebBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  IDZWebBrowser
+//
+//  Created by idz on 11/16/13.
+//  Copyright (c) 2013 iOSDeveloperZone.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IDZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IDZAppDelegate class]));
+    }
+}
